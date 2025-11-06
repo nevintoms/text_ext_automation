729a1d0517ba6646b2fa64d0271c606c90e89f83
Nevin Toms <32118139+nevintoms@users.noreply.github.com>
2025-11-06 15:57:25 -0500

    Delete upload_s3.py

--- upload_s3.py
+++
@@ -1,7 +0,0 @@
-# pick a bucket name (must be globally unique)
-export AWS_DEFAULT_REGION=us-east-1
-export BUCKET=discharge-ingest-nevin-123
-aws s3 mb s3://$BUCKET
-
-# upload your PDF (change the local file path)
-aws s3 cp ./discharge.pdf s3://$BUCKET/ingest/discharge.pdf
